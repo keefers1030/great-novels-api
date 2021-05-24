38e2b57a60d7b6e7354016031655ad3a524e2d7c
Keith March Mistler <kmistler@gmail.com>
2021-05-23 20:06:19 -0400

    added novels model

--- models/genres.js
+++ models/genres.js
@@ -1,5 +1,5 @@
 const genres = (connection, Sequelize) => {
-  return connection.define('authors', {
+  return connection.define('genres', {
     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
     name: { type: Sequelize.STRING },
   }, { paranoid: true })

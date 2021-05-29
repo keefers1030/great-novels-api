4403a5d00fdc3b2d38a92180af038fb6a4a56fa7
Keith March Mistler <kmistler@gmail.com>
2021-05-29 11:06:50 -0400

    added migration for genres table

--- migrations/20210529144922-initial-table-creation.js
+++ migrations/20210529144922-initial-table-creation.js
@@ -2,22 +2,32 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable('authors', {
+    await queryInterface.createTable('authors', {
       id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
       nameFirst: { type: Sequelize.STRING },
       nameLast: { type: Sequelize.STRING },
-      updatedAt: { type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')},
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      },
       deletedAt: { type: Sequelize.DATE }
-})
+    })
+
+    await queryInterface.createTable('genres', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      },
+      deletedAt: { type: Sequelize.DATE }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('genres')
+    await queryInterface.dropTable('authors')
   }
 }
